fb130624f1659f3999f94f5f98966e54648e0c2a
Osagie Erhabor <erhaborosagie@gmail.com>
2018-07-01 00:30:22 +0100

    Service Worker and Indexeddb working

--- js/apps.js
+++ js/apps.js
@@ -1,12 +1,17 @@
-const url = `https://free.currencyconverterapi.com/api/v5/countries `;
 let app={
     cF:document.querySelector('#convertFrom'),
     aF:document.querySelector('#amountFrom'),
     cTo:document.querySelector('#convertTo'),
-    result : document.querySelector('#result')
+    converted : document.querySelector('#converted'),
+    countries:['a']
 };
-
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js')
+    });
+}
 //Get the countries
+const url = `https://free.currencyconverterapi.com/api/v5/countries `;
 fetch(url).then((response)=>{
     return response.json()
 }).then((myJson)=>{
@@ -24,20 +29,79 @@ fetch(url).then((response)=>{
 }).catch((e)=>{
     console.log(e);
 });
-
 //This function is called when the user clicks the submit button to convert the currency
 changeHandler = () => {
     let cF = encodeURIComponent(app.cF.value);
     let aF = encodeURIComponent(app.aF.value);
     let cTo = encodeURIComponent(app.cTo.value);
     const query = `${cF}_${cTo}`;
-    const url1 = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`
+    const query2 = `${cTo}_${cF}`;
+    // call fetch so as to get latest conversion rate
+    const url1 = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
     fetch(url1).then((response)=>{
+
         return response.json()
-    }).then((myJson)=>{
+
+    }).then((myJson)=>{  
+
         ans = myJson[query]['val'] * aF;
-        result.innerHTML = ans;
+        converted.innerHTML = ans;
+
+        getRate(myJson, 'putData', query, query2, aF) 
+        
     }).catch((e)=>{
-        console.log(e);
+
+        getRate(' No internet', 'getData', query, query2, aF) 
+
     })
+}
+
+
+function getRate(myJson, mode, query, query2, aF) {
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    if (!window.indexedDB) {
+        ans = myJson[query]['val'] * aF;
+        converted.innerHTML = ans;
+        return;
+    }
+    let request = window.indexedDB.open("currencyConverter", 1);
+
+    request.onupgradeneeded=(e)=>{
+        let db = request.result;
+        store = db.createObjectStore("country", { keyPath: "countryId" });
+        let Index = store.createIndex("currencyId", "currencyId", {unique: true});
+    };
+
+    request.onsuccess=(e)=>{
+        db = request.result;
+        let tx = db.transaction("country", "readwrite");
+        let store =tx.objectStore("country");
+
+        if (mode==='putData') {
+            let message = myJson[query]['val'];
+            let message2 = 1/message
+            store.put({countryId:query,rate:message});
+            store.put({countryId:query2,rate:message2});
+        } else {
+            req =store.get(query)
+            req.onsuccess=(e) =>{
+                let matching = req.result;
+                if (matching !== undefined) {
+                    converted.innerHTML =matching['rate'] * aF;
+                } else {
+                    // No match was found.
+                    ans = 'Internet connection needed';
+                    converted.innerHTML = ans;
+                }
+            }
+        }
+        tx.oncomplete = () => {
+            db.close();
+        }
+    }
+
+    request.onerror=(e) => {
+        ans = 'Please try again later';
+        converted.innerHTML = ans;
+    }
 }
